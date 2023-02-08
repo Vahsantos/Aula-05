127ea1008414d8bdc1c8888f9485f9c41e08a769
Gabriel Lima <gabriel.lyma16@gmail.com>
2023-02-08 19:32:07 -0300

    aula 5

--- atividades/ex05.js
+++ atividades/ex05.js
@@ -1,25 +1,74 @@
-let animal = prompt('Digite o tipo de animal para o qual deseja saber os serviços disponíveis em nosso PetShop:').toLocaleLowerCase()
-
-switch (animal) {
-    case 'cachorro':
-    case 'gato':
-    case 'coelho':
-        alert(`Para ${animal}s oferecemos os serviços de banho, tosa e também temos rações.`)
-        break
-
-    case 'cavalo':
-    case 'cabra':
-    case 'jumento':
-        alert(`Para ${animal}s oferecemos os serviços de veterinário especializado e também temos rações.`)
-        break
-    case 'passarinho':
-        alert(`Para ${animal}s possuímos o serviço de implante de rastreador e tambpem temos rações.`)
-        break
-    case 'rato':
-        alert(`para ${animal}s não possuímos serviços, porém oferecemos gaiolas especiais e rações.`)
-        break
-    default:
-        alert(`Desculpe, não realizamos atendimento para ${animal}s em nosso PetShop. Ficamos gratos por ter procurado nosso atendimento!`)
+function validaCpf(cpf) {
+    while (isNaN(cpf) || cpf == '' || cpf.length != 11 || validaDigitoUm(cpf) == false || validaDigitoDois(cpf) == false) {
+        cpf = (' Valor invalido. Digite um CPF com 11 dígitos:')
+    } return true
 }
 
-window.location.reload()
+function validaDigitoUm(cpf) {
+    let testeUm = ''
+    let checkUm = 0
+    let somaUm = 0
+
+    for (i = 0; i < 9; i++) {
+        somaUm += Number(cpf[i]) * (10 - i)
+    }
+
+    checkUm = 11 - (somaUm % 11)
+
+    if (checkUm >= 10) {
+        testeUm = 0
+    } else {
+        testeUm = checkUm
+    }
+
+    if (testeUm == cpf[9]) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function validaDigitoDois(cpf) {
+    let testeDois = ''
+    let checkDois = 0
+    let somaDois = 0
+
+    for (j = 0; j < 10; j++) {
+        somaDois += Number(cpf[j]) * (11 - j)
+    }
+
+    checkDois = 11 - (somaDois % 11)
+
+    if (checkDois >= 10) {
+        testeDois = 0
+    } else {
+        testeDois = checkDois
+    }
+
+    if (testeDois == cpf[10]) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function formataCpf(cpf) {
+    let novoCpf = cpf.slice(0, 3) + '.' + cpf.slice(3, 6) + '.' + cpf.slice(6, 9) + '-' + cpf.slice(9, 11)
+    return novoCpf
+}
+
+let listaCpfs = []
+let listaFinal = []
+
+for (c = 1; c <= 5; c++) {
+    let cpf = prompt(`Digite o ${c}º número de CPF com 11 dígitos, apenas números e incluindo os zeros que seja válido:`)
+    if (validaCpf(cpf)) {
+        listaCpfs.push(cpf)
+    }
+}
+
+for (x=0; x<listaCpfs.length; x++) {
+    listaFinal.push(formataCpf(listaCpfs[x]))
+}
+
+alert(listaFinal)
